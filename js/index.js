5ed5f6bfc798d0e79810eef978cd9e5e61af5555
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2025-05-20 13:03:35 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,56 +8,56 @@ var tries = 00;
 var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
 var buttonPause = document.getElementById('button-pause');
-var Interval;
-var isProcessing = false; // Флаг для блокировки открытия более двух карт
+
+var Interval ;
 var images = [
     '1', '2', '3', '4', '5', '6', '7', '8',  '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'
 ];
+
 var clone = images.slice(0); // duplicate array
 var cards = images.concat(clone); // merge to arrays
+
 // Shufffel function
 function shuffle(o){
   for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i],   o[i] = o[j], o[j] = x);
   return o;
 }
 shuffle(cards);
+
 for (var i = 0; i < cards.length; i++) {
   card = document.createElement('div');
   card.dataset.item = cards[i];
   card.dataset.view = "card";
   myCards.appendChild(card);
+
   card.onclick = function () {
-    // Проверяем, что не обрабатываем сейчас карты и открыто менее двух карт
-    if (!isProcessing && document.getElementsByClassName('flipped').length < 2) {
-        if (this.className != 'flipped' && this.className != 'correct'){
-            this.className = 'flipped';
-            var result = this.dataset.item;
-            resultsArray.push(result);
-            clearInterval(Interval);
-            Interval = setInterval(startTimer, 10);
-        }
+    if (this.className != 'flipped' && this.className != 'correct'){
+        this.className = 'flipped';
+        var result = this.dataset.item;
+        resultsArray.push(result);
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
     }
-    
-    if (resultsArray.length == 2) {
-      isProcessing = true; // Устанавливаем флаг обработки
+
+
+    if (resultsArray.length > 1) {
       if (resultsArray[0] === resultsArray[1]) {
         check("correct");
         counter ++;
         win();
         resultsArray = [];
-        setTimeout(function() {
-          isProcessing = false; // Снимаем флаг после обработки
-        }, 600);
       } else {
         check("reverse");
         resultsArray = [];
-        setTimeout(function() {
-          isProcessing = false; // Снимаем флаг после обработки
-        }, 600);
       }
+
     }
+
   }
+
 };
+
+
 window.onload = function () {
     span = document.getElementById('triesid');
     var tries = 00;
@@ -70,42 +70,64 @@ window.onload = function () {
         span.innerHTML = Math.floor(tries / 2);
     }
 }
+
+
+
+
 var check = function(className) {
   var x = document.getElementsByClassName("flipped");
+
   setTimeout(function() {
+
     for(var i = (x.length - 1); i >= 0; i--) {
       x[i].className = className;
     }
+
   },500);
+
 }
+
+
 var win = function () {
+
   if(counter === 18) {
     clearInterval(Interval);
       text.innerHTML = "Счет: " + (5000 - (seconds + tries));
   }
+
 }
+
 function change_color(obj)
 {
     obj.value && (document.body.style.backgroundColor = obj.value);
 }
+
 buttonPause.onclick = function() {
     clearInterval(Interval);
 }
+
+
 function startTimer () {
   tens++;
+
   if(tens < 9){
     appendTens.innerHTML = "0" + tens;
   }
+
   if (tens > 9){
     appendTens.innerHTML = tens;
+
   }
+
   if (tens > 99) {
     seconds++;
     appendSeconds.innerHTML = "0" + seconds;
     tens = 0;
     appendTens.innerHTML = "0" + 0;
   }
+
   if (seconds > 9){
     appendSeconds.innerHTML = seconds;
   }
+
 }
